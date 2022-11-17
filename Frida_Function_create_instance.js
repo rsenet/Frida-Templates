d2b7ec4bcabd2db6df6ce7a7a2eff0981b2137ef
Régis S <regis@shares.io>
2022-11-17 16:25:15 +0100

    first commit

---
+++ Frida_Function_create_instance.js
@@ -0,0 +1,11 @@
+Java.perform(function () {
+    Java.choose("PACKAGE_NAME", {
+        onMatch : function(instance){ 
+                console.log("Found instance: " + instance);
+
+                // Extra Code Goes Here
+        },
+
+    onComplete:function(){}
+    });    
+});
---
+++ Frida_Standard.js
@@ -0,0 +1,8 @@
+setImmediate(function(){
+    Java.perform(function() { 
+        var currentApplication = Java.use("android.app.ActivityThread").currentApplication();
+        var context = currentApplication.getApplicationContext();  
+
+        // Extra Code Goes Here
+    });
+});
