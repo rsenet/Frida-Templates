33fb0209d64d80d56625c510c0954ac519bb20f7
Régis S <regis@shares.io>
2023-01-12 17:31:26 +0100

    Add more details regarding functions

--- Frida_Function_Hooking.js
+++ Frida_Function_Hooking.js
@@ -3,10 +3,17 @@ Java.perform(function ()
     // Name of the class to start hooking.
     var MainActivity = Java.use("com.example.name.classname");
 
+    // Simple implementation
     MainActivity.function_name.implementation = function() 
     {
         // write the modified function code here
         return false;
     }
-    console.log("function simply returned.")
+
+    // Simple implementation with overloading
+    MainActivity.function_name.overload("java.lang.String", "java.lang.String").implementation = function() 
+    {
+        // write the modified function code here
+        return false;
+    }    
 });
--- Frida_Function_create_instance.js
+++ Frida_Function_create_instance.js
@@ -2,13 +2,18 @@ Java.perform(function ()
 {
     Java.choose("PACKAGE_NAME", 
     {
+        // OnMatch gets called with 1 or more arguments once Frida finds a match for your request
         onMatch : function(instance)
         { 
-                console.log("Found instance: " + instance);
+                console.log("Instance found: " + instance);
+                console.log("Instance found: " + instance.toString());
 
                 // Extra Code Goes Here
         },
+        // onComplete gets called when Frida finished iterating through possible matches
+        onComplete:function()
+        {
 
-    onComplete:function(){}
-    });    
+        }
+    });
 });
