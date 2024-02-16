27e49dda3180eca2376be5586671e6ce06a498c7
rsenet <regis.senet@orhus.fr>
2024-02-16 12:36:02 +0100

    Add way to parse arguments

--- Frida_Android_Hooking_Native.js
+++ Frida_Android_Hooking_Native.js
@@ -7,6 +7,18 @@ Interceptor.attach(TARGET_ADDRESS,
         console.log('Entering function');
         /*
             MODIFY OR LOG ARGUMENTS
+
+            Read X argument as ASCII
+            var argX = Memory.readCString(args[X]);
+            
+            Read X argument as UTF-8
+            var argX = Memory.readUtf8String(args[X]);
+
+            Read X argument as UTF-16
+            var argX = Memory.readUtf16String(args[X]);
+
+            Read X argument as ANSI
+            var argX = Memory.readAnsiString(args[X]);
         */
     },
 
