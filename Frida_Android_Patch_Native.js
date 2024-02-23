2e8e944d6a5aa2f266e846c85d9cd7a9dd41b964
Régis S <regis@shares.io>
2024-02-23 15:42:14 +0100

    Update Android_Patch_Native.js with additional information

--- Frida_Android_Patch_Native.js
+++ Frida_Android_Patch_Native.js
@@ -1,19 +1,31 @@
 // All capital letters must be replaced
 
-// X86
-// Usefull link: https://frida.re/docs/javascript-api/#x86writer
-var INSTRUCTION_PTR = Module.getBaseAddress(LIB).add(INSTRUCTION_ADDR - 0x00010000);
-Memory.protect(INSTRUCTION_PTR, 0x1000, "rwx");
-var WRITER = new X86Writer(INSTRUCTION_ADDRESS);
+// X86 //
+// REAL_INSTRUCTION_ADDR = INSTRUCTION_ADDR (Ghidra) - SEGMENT_START_ADDR (by default: 0x00010000)
+var INSTRUCTION_PTR = Module.getBaseAddress(LIB_SO).add(REAL_INSTRUCTION_ADDR);
 
-// ARM64
-// Usefull link: https://frida.re/docs/javascript-api/#arm64writer
-var INSTRUCTION_PTR = Module.getBaseAddress(LIB).add(INSTRUCTION_ADDR);
+// Patch Memory Protection with Read Write and eXecute permissions
 Memory.protect(INSTRUCTION_PTR, 0x1000, "rwx");
-var WRITER = new Arm64Writer(INSTRUCTION_ADDRESS);
+var WRITER = new X86Writer(INSTRUCTION_PTR);
+// END X86 //
+
+
+// ARM64 //
+// REAL_INSTRUCTION_ADDR = INSTRUCTION_ADDR (Ghidra) - SEGMENT_START_ADDR (by default: 0x00010000)
+var INSTRUCTION_PTR = Module.getBaseAddress(LIB_SO).add(REAL_INSTRUCTION_ADDR);
+
+// Patch Memory Protection with Read Write and eXecute permissions
+Memory.protect(INSTRUCTION_PTR, 0x1000, "rwx");
+var WRITER = new Arm64Writer(INSTRUCTION_PTR);
+// END ARM64 //
 
 try
 {
+	/* Usefull links
+		X86   - Usefull link: https://frida.re/docs/javascript-api/#x86writer
+		ARM64 - Usefull link: https://frida.re/docs/javascript-api/#arm64writer
+	*/
+
 	// Flush the changes (apply the changes to the memory)
 	WRITER.flush();
 
