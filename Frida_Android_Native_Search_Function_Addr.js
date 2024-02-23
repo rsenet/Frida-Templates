d44282cbe2c84f5cf179650f1041d3af4137d10e
rsenet <regis.senet@orhus.fr>
2024-02-23 16:03:05 +0100

    Renamed files & Add Native_Search_Function_Addr template

---
+++ Frida_Android_Native_Search_Function_Addr.js
@@ -0,0 +1,32 @@
+// All capital letters must be replaced
+
+// Default return value for SEARCH_FUNCTION_ADDR
+var SEARCH_FUNCTION_ADDR = 0x0;
+ 
+// Getting all exports out of the libnative-lib.so
+var exportedFunctions = Module.enumerateExports(LIB_SO)
+ 
+// Iterating over this array trying to find SEARCHED_FUNCTION
+exportedFunctions.forEach(function(element, index)
+{
+    // Looking for the function that contains SEARCHED_FUNCTION
+    if(element.name.includes(SEARCHED_FUNCTION)) 
+    {
+        console.log("Name = " + element.name);
+        console.log("Pointer = " + element.address);
+
+        // Getting the address of the desired function
+        SEARCH_FUNCTION_ADDR = element.address;
+    }
+})
+ 
+Interceptor.attach(SEARCH_FUNCTION_ADDR, 
+{ 
+    onEnter: function(args) 
+    {
+    },
+
+    onLeave: function(ret)
+    {
+    }
+})
