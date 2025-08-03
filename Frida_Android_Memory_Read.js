83a381a521584e409520b10cfca22784cb981c09
rsenet <25456431+rsenet@users.noreply.github.com>
2025-08-03 17:11:50 +0200

    Add Frida_Android_Memory_Read script

---
+++ Frida_Android_Memory_Read.js
@@ -0,0 +1,32 @@
+// All capital letters must be replaced
+
+const lib = Process.getModuleByName(LIB_SO);
+console.log("LIB_SO loaded at:", lib.base);
+
+// Base64 version of your pattern. Ex: "464c4147" for "FLAG"
+const pattern = "PATTERN"; 
+
+Memory.scan(lib.base, lib.size, pattern,
+{
+    onMatch: function (address, size)
+    {
+        console.log("[+] Match found at:", address);
+
+        try
+        {
+            var flagString = address.readCString();
+            console.log("[+] Flag:", flagString);
+        }
+        catch (err)
+        {
+            console.error("[!] Failed to read string:", err);
+        }
+
+        console.log(hexdump(address, { length: 64 }));
+    },
+
+    onComplete: function ()
+    {
+        console.log("[*] Memory scan complete");
+    }
+});
